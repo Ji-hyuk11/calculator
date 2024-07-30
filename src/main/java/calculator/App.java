5c53ac686f63fa61c129e42599c59d1febbe28f2
JiHyuk SON <shonjjun@gmail.com>
2024-07-30 20:44:54 +0900

    Level - 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,8 @@ public class App {
         Scanner sc = new Scanner(System.in); // 스캐너 생성
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt(); // 첫번째 정수를 입력받습니다.
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char enterOperator = sc.next().charAt(0); // 사용자로부터 입력받은 문자열을 변수에 할당합니다.
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt(); // 두번째 정수를 입력받습니다.
     }
